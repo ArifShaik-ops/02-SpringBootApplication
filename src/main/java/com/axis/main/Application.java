e9b8df4041f50a1198f5d46cf015809b9bfccf83
arifshaik-ops <arif.shaik.java@gmail.com>
2020-08-19 18:22:34 +0530

    Application.Java Modified

--- src/main/java/com/axis/main/Application.java
+++ src/main/java/com/axis/main/Application.java
@@ -2,12 +2,17 @@ package com.axis.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	ConfigurableApplicationContext context=	SpringApplication.run(Application.class, args);
+	context.close();
+		
+		
+		
 	}
 
 }
