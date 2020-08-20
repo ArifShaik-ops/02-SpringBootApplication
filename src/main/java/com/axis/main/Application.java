d67b5a18eeee9210f07eee0f9b8537fdcd090808
arifshaik-ops <arif.shaik.java@gmail.com>
2020-08-20 12:01:06 +0530

    Added Variable

--- src/main/java/com/axis/main/Application.java
+++ src/main/java/com/axis/main/Application.java
@@ -9,6 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 	ConfigurableApplicationContext context=	SpringApplication.run(Application.class, args);
+	int a=10;
 	// Changes for HIS-134
 	context.close();
 		
